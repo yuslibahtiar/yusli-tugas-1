da73c1e0850221d9a431810146de06d2dde84156
yuslibahtiar <48341508+yuslibahtiar@users.noreply.github.com>
2019-03-08 11:35:28 +0700

    Add files via upload

---
+++ yuslibahtiar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fan Fan
+ */
+public class yuslibahtiar {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        System.out.println("Hello, I'am yusli_baktiar_17090036 From 4B class ");
+    }
+
+    }
+    
